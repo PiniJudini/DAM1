3fd74ebb9c97ee6415ddfa8fc4c96d479fe30101
Marcos Pineros Quero <elpinersbro@gmail.com>
2026-03-18 09:32:43 +0100

    Add files via upload

--- Programación/TercerTrimestre/Boletin25_Ejercicio4/Main.java
+++ Programación/TercerTrimestre/Boletin25_Ejercicio4/Main.java
@@ -3,5 +3,7 @@ package Boletin25_Ejercicio4;
 public class Main {
 	public static void main(String[] args) {
 		Tarea.leerFichero("tareas.txt");
+		Tarea.listarTareas();
+		Tarea.ordenarTareas();
 	}
 }
